63b048cccce17b81a5c46a800c531ae830e02147
HHNebula <sungsoomr@icloud.com>
2022-12-20 09:15:45 +0900

    Update

---
+++ src/main/java/com/toy/spring/toy_spring/servlets/CreateSessionServlet.java
@@ -0,0 +1,38 @@
+package com.toy.spring.toy_spring.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet(urlPatterns = "/session/CreateSessionServlets")
+public class CreateSessionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // display
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<div>Create Session Servlets</div>");
+        // login
+        if (username.equals("SOO") && password.equals("5525")) {
+            HttpSession httpSession = request.getSession();
+            httpSession.setAttribute("username", username);
+            httpSession.setAttribute("password", password);
+            printWriter.println("<div>" + username + ", " + password + "</div>");
+        } else {
+            printWriter.println("<div>Faild</div>");
+        }
+        printWriter.close();
+    }
+
+}
